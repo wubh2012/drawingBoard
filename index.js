1c3cc85d8f1928e5f2b18dd3528b55d1c32361ce
wubh2012 <wubh2012@163.com>
2019-04-02 15:03:19 +0800

    重构

--- index.js
+++ index.js
@@ -2,71 +2,9 @@ var log = console.log.bind(console)
 
 var mycanvas = document.querySelector('#mycanvas')
 var ctx = mycanvas.getContext('2d')
-
-var isMoveing = false
 var earserEnable = false
-var lastPoint = {
-    x: undefined,
-    y: undefined,
-}
 
-setCanvasSize()
-
-window.onresize = function(){
-    setCanvasSize()
-}
-
-mycanvas.onmousedown = function (event) {
-    log('mouse down', event)
-    isMoveing = true
-    var x = event.x
-    var y = event.y
-    lastPoint = {
-        x: x,
-        y: y,
-    }
-}
-
-mycanvas.onmousemove = function (event) {
-    if (isMoveing) {
-        log('mouse move', event)
-        var x = event.x
-        var y = event.y
-        var newPoint = {
-            x: x,
-            y: y,
-        }
-        if(earserEnable){
-            // 擦除
-            ctx.clearRect(lastPoint.x, lastPoint.y, 50, 50);
-        }else{
-            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-        }
-        lastPoint = newPoint
-        
-    }
-}
-
-mycanvas.onmouseup = function (event) {
-    isMoveing = false
-    log('mouse up', event)
-}
-
-
-var earserButton = document.querySelector('.eraser')
-var brushButton = document.querySelector('.brush')
-var actionsContainer = document.querySelector('.actions')
-earserButton.addEventListener('click', function(){
-    earserEnable = true
-    actionsContainer.classList.add('x')
-})
-brushButton.addEventListener('click', function(){
-    earserEnable = false
-    actionsContainer.classList.remove('x')
-
-})
-
-function drawLine(x1, y1, x2, y2) {
+var drawLine = function (x1, y1, x2, y2) {
     ctx.beginPath()
     ctx.moveTo(x1, y1)
     ctx.lineTo(x2, y2)
@@ -75,12 +13,87 @@ function drawLine(x1, y1, x2, y2) {
     ctx.closePath()
 }
 
-
-function setCanvasSize(){
-    
-    var width = document.documentElement.clientWidth
-    var height = document.documentElement.clientHeight
-    log(`width: ${width}, height: ${height}`)
-    mycanvas.width = width
-    mycanvas.height = height
+var clearArea = function (x, y) {
+    ctx.clearRect(x, y, 20, 20);
 }
+
+var listenerCanvas = function (canvas) {
+    var isMoveing = false
+    var lastPoint = {
+        x: undefined,
+        y: undefined,
+    }
+    canvas.onmousedown = function (event) {
+        log('mouse down', event)
+        isMoveing = true
+        var x = event.x
+        var y = event.y
+        lastPoint = {
+            x: x,
+            y: y,
+        }
+    }
+
+    canvas.onmousemove = function (event) {
+        if (!isMoveing) {
+            return
+        }
+        var x = event.x
+        var y = event.y
+        var newPoint = {
+            x: x,
+            y: y,
+        }
+        if (earserEnable) {
+            // 擦除
+            clearArea(lastPoint.x, lastPoint.y)
+        } else {
+            // 画线
+            drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        }
+        lastPoint = newPoint
+
+    }
+
+    canvas.onmouseup = function (event) {
+        isMoveing = false
+    }
+
+}
+
+var listenerActions = function () {
+    var earserButton = document.querySelector('.eraser')
+    var brushButton = document.querySelector('.brush')
+    var actionsContainer = document.querySelector('.actions')
+    earserButton.addEventListener('click', function () {
+        earserEnable = true
+        actionsContainer.classList.add('x')
+    })
+    brushButton.addEventListener('click', function () {
+        earserEnable = false
+        actionsContainer.classList.remove('x')
+    })
+}
+
+var autoSetCanvasSize = function (canvas) {
+    var setCanvasSize = function () {
+        var width = document.documentElement.clientWidth
+        var height = document.documentElement.clientHeight
+        log(`width: ${width}, height: ${height}`)
+        canvas.width = width
+        canvas.height = height
+    }
+    setCanvasSize()
+
+    window.onresize = function () {
+        setCanvasSize()
+    }
+}
+
+var __main = function () {
+    autoSetCanvasSize(mycanvas)
+    listenerCanvas(mycanvas)
+    listenerActions()
+}
+
+__main()
